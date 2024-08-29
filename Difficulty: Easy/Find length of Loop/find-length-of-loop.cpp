68d47de88a42586558f9291e265c10ef60394c02
Khushi Srivastava <98648413+Khushisrivastava9@users.noreply.github.com>
2024-08-29 22:50:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find length of Loop/find-length-of-loop.cpp
+++ Difficulty: Easy/Find length of Loop/find-length-of-loop.cpp
@@ -1,65 +1,37 @@
 //{ Driver Code Starts
-// driver code
-
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node
-{
+struct Node {
     int data;
-    Node* next;
-    
-    Node(int val)
-    {
-        data = val;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
         next = NULL;
     }
 };
 
-void loopHere(Node* head, Node* tail, int position)
-{
-    if(position==0) return;
-    
-    Node* walk = head;
-    for(int i=1; i<position; i++)
+void printList(Node *node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+void loopHere(Node *head, Node *tail, int position) {
+    if (position == 0)
+        return;
+
+    Node *walk = head;
+    for (int i = 1; i < position; i++)
         walk = walk->next;
     tail->next = walk;
 }
 
-int countNodesinLoop(Node* head);
-
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n, num;
-        cin>>n;
-        
-        Node *head, *tail;
-        cin>> num;
-        head = tail = new Node(num);
-        
-        for(int i=0 ; i<n-1 ; i++)
-        {
-            cin>> num;
-            tail->next = new Node(num);
-            tail = tail->next;
-        }
-        
-        int pos;
-        cin>> pos;
-        loopHere(head,tail,pos);
-        
-        cout<< countNodesinLoop(head) << endl;
-    }
-	return 0;
-}
 
 // } Driver Code Ends
-
-
 /*
 
 struct Node {
@@ -72,11 +44,12 @@ struct Node {
 };
 
 */
-
-//Function to find the length of a loop in the linked list.
-int countNodesinLoop(struct Node *head)
-{
-    Node* temp=head;
+class Solution {
+  public:
+    // Function to find the length of a loop in the linked list.
+    int countNodesinLoop(Node *head) {
+        // Code here
+        Node* temp=head;
     unordered_map<Node*,bool> vis;
     int cnt=0;
     while(temp!=NULL){
@@ -92,4 +65,39 @@ int countNodesinLoop(struct Node *head)
         temp=temp->next;
     }
     return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int k;
+        cin >> k;
+        cin.ignore();
+        struct Node *head = new Node(arr[0]);
+        struct Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+        loopHere(head, tail, k);
+
+        Solution ob;
+        cout << ob.countNodesinLoop(head) << endl;
+    }
+    return 0;
 }
+
+// } Driver Code Ends
