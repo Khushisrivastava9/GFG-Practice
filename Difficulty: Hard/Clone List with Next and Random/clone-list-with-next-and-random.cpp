9bc7556974c273712cbf5ac6e2790a7db16dda17
Khushi Srivastava <98648413+Khushisrivastava9@users.noreply.github.com>
2025-01-23 12:05:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Clone List with Next and Random/clone-list-with-next-and-random.cpp
@@ -0,0 +1,200 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node* random;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+void print(Node* root) {
+    map<Node*, int> link;
+    Node* temp = root;
+    for (int i = 0; temp != NULL; i++) {
+        link[temp] = i;
+        temp = temp->next;
+    }
+    temp = root;
+    cout << "[";
+    while (temp->next != NULL) {
+        if (!temp->random) {
+            cout << "[" << temp->data << ", "
+                 << "NULL"
+                 << "], ";
+        } else
+            cout << "[" << temp->data << ", " << link[temp->random] + 1 << "], ";
+        temp = temp->next;
+    }
+    if (!temp->random) {
+        cout << "[" << temp->data << ", "
+             << "NULL"
+             << "]]\n";
+    } else
+        cout << "[" << temp->data << ", " << link[temp->random] + 1 << "]]\n";
+}
+
+bool validation(Node* res, map<Node*, int>& orgAddress) {
+    Node* temp = res;
+    for (int i = 0; temp != NULL; i++) {
+        if (orgAddress.find(temp) != orgAddress.end()) {
+            return false;
+        }
+        if (orgAddress.find(temp->random) != orgAddress.end()) {
+            return false;
+        }
+        temp = temp->next;
+    }
+    return true;
+}
+
+Node* buildLinkedList(vector<pair<int, int>>& v, map<Node*, int>& orgAddress) {
+    vector<Node*> address(v.size());
+    Node* head = new Node(v[0].first);
+    address[0] = head;
+    Node* temp = head;
+    orgAddress[head] = 0;
+    for (int i = 1; i < v.size(); i++) {
+        Node* newNode = new Node(v[i].first);
+        orgAddress[newNode] = i;
+        address[i] = newNode;
+        temp->next = newNode;
+        temp = temp->next;
+    }
+    temp = head;
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i].second != -1) {
+            temp->random = address[v[i].second - 1];
+        }
+        temp = temp->next;
+    }
+
+    return head;
+}
+
+bool validateInput(map<Node*, int>& orgAddress, Node* head, vector<pair<int, int>>& v) {
+    vector<Node*> address(v.size());
+    Node* temp = head;
+    for (int i = 0; i < v.size(); i++) {
+        if (orgAddress.find(temp) == orgAddress.end() || orgAddress[temp] != i) {
+            return false;
+        }
+        address[i] = temp;
+        temp = temp->next;
+    }
+    if (temp != NULL) {
+        return false;
+    }
+    temp = head;
+    for (int i = 0; i < v.size(); i++) {
+        int value = v[i].first;
+        int randomIndex = v[i].second;
+
+        if (randomIndex == -1) {
+            if (temp->random != NULL) {
+                return false;
+            }
+        } else {
+            Node* tempNode = address[randomIndex - 1];
+            if (temp->random != tempNode) {
+                return false;
+            }
+        }
+        temp = temp->next;
+    }
+    return true;
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    Node *cloneLinkedList(Node *head)
+    {
+        //Write your code here
+        Node* clonell=new Node(0);
+        Node* temp=head;
+        Node* clonehead=clonell;
+        //make the clone ll with next  pointers as in original ll
+        while(temp!=NULL){
+            clonehead->next=new Node(temp->data);
+            temp=temp->next;
+            clonehead=clonehead->next;
+        }
+        //now let' also clone the random pointer so how to do this
+        //create a map that maps node in original ll to the clonell
+        unordered_map<Node*,Node*> mp;
+        Node* originalnode=head;
+        Node* clonenode=clonell->next;
+        while(originalnode!=NULL && clonenode!=NULL){
+            mp[originalnode]=clonenode;
+            originalnode=originalnode->next;
+            clonenode=clonenode->next;
+        }
+        
+        //now copy the random pointer of original to clone
+        originalnode=head;
+        clonenode=clonell->next;
+        while(originalnode!=NULL){
+            clonenode->random =mp[originalnode->random ];
+            originalnode=originalnode->next;
+            clonenode=clonenode->next;
+        }
+        return clonell->next;
+    }
+
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        vector<pair<int, int>> v(n); // node data, random node number
+        for (int i = 0; i < n; i++) {
+            int x;
+            string y;
+            cin >> x >> y;
+            if (y == "NULL" || y == "N" || y == "null" || y == "n" || y == "Null") {
+                v[i] = {x, -1};
+            } else {
+                v[i] = {x, stoi(y)};
+            }
+        }
+        map<Node*, int> orgAddress;
+        Node* head = buildLinkedList(v, orgAddress);
+
+        Solution ob;
+        Node* res = ob.cloneLinkedList(head);
+        // check if input modified.
+        if (validateInput(orgAddress, head, v)) {
+            if (validation(res, orgAddress)) {
+                print(res);
+            } else {
+                cout << "Pointing to the original list\n";
+            }
+        } else {
+            cout << "Input list modified\n";
+        }
+        cout << "~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
